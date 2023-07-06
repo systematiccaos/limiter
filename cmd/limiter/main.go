0e6ccc3bc7c361168c01933e0e0c221d9907c36b
caos <sysexect@gmail.com>
2023-07-06 15:37:11 +0200

    limiter

--- cmd/limiter/main.go
+++ cmd/limiter/main.go
@@ -40,6 +40,7 @@ func main() {
 		if last_total_power < 20 && last_total_power != 0.0 {
 			limit = last_total_power + last_solar_power - 20
 		}
+		logrus.Println(time.Since(last_update))
 		if oldlimit != limit && time.Since(last_update) < 10*time.Second {
 			logrus.Printf("limit: %f", limit)
 			logrus.Printf("last_total_power: %f", last_total_power)
